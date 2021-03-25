9cf3c12267d649fb5102b69382ca43edc78348ed
sojeong2020 <73712403+sojeong2020@users.noreply.github.com>
2021-03-25 17:58:00 +0000

    modify App.jsx index.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return <div />;
+  const [count, setCount] = useState(0);
+
+  function increase() {
+    setCount(count + 1);
+  }
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={increase}>+</button>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
-let count = 0;
-ReactDOM.render(
-  <div className="container">
-    <h1>{count}</h1>
-    <button>+</button>
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
